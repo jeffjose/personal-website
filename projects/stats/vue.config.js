766f3cb08cebdada56d0e048ad3bdb36b7e2bade
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-03-08 23:59:29 -0800

    Initial add of project2

---
+++ projects/stats/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: 'stats/',
+  outputDir: '../../dist/stats/',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
