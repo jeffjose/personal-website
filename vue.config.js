2bd55cae23f97091b31b7c48c4ee60fab0ca953b
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-03-13 22:38:55 -0700

    different values for serving path based on env

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: 'homepage/',
-  outputDir: 'dist/homepage',
-  indexPath: '../index.html'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "homepage/" : "/",
+  outputDir: process.env.NODE_ENV === "production" ? "dist/homepage" : "dist/",
+  indexPath:
+    process.env.NODE_ENV === "production" ? "../index.html" : "index.html"
+};
