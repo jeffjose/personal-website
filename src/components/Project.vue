b0e2adb27bdeeebf85178b9e095af3cb341423d5
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-03-13 22:33:31 -0700

    Progress on homepage

---
+++ src/components/Project.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+.column.wrapper
+  p.toptext {{title}}
+</template>
+
+<script>
+export default {
+  name: "Project",
+  props: {
+    title: String,
+    url: String,
+    desc: String
+  }
+};
+</script>
+
+<style scoped lang="sass">
+.wrapper
+  height: 100px
+  background-color: darken(rgb(246, 249, 252), 1%)
+  border-color: darken(rgb(246, 249, 252), 3%)
+  border-width: 1px
+  border-style: solid
+  margin: 10px
+  border-radius: 4px
+  color: darken(rgb(246, 249, 252), 35%)
+  pointer-events: all
+
+  & p
+    transition: all 80ms ease-in-out
+    margin-top: -10px
+    opacity: 0
+
+  &:hover p
+    margin-top: 0px
+    opacity: 1
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,15 @@
         .container
           p.title Jeffrey Jose
           p.subtitle Product Manager at Google
+          .columns
+            Project(title="Interactive Statistical Distribution", url="/statsviz")
+            Project(title="Subnet/IP Calculator", url="/ip-calculator")
+            Project(title="Colors", url="/colors")
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
 import Background from "@/components/Background.vue";
+import Project from "@/components/Project.vue";
 
 export default {
   name: "home",
@@ -18,7 +22,7 @@ export default {
     document.title = document.location.hostname;
   },
   components: {
-    Header,
+    Project,
     Background
   }
 };
@@ -33,12 +37,13 @@ export default {
 .title
   @extend .title
   font-weight: 400
+  color: darken(rgb(246, 249, 252), 65%)
 
 .subtitle
   @extend .subtitle
   font-weight: 300
+  color: darken(rgb(246, 249, 252), 65%)
 
 .welcome
   //background: radial-gradient(circle at 10% 20%, rgb(223, 181, 251) 0%, rgb(251, 181, 246) 35.5%, rgb(251, 207, 181) 63.5%, rgb(250, 253, 197) 90%)
-
 </style>
