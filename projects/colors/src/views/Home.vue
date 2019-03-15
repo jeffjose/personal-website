813bca28f3c6bbbe957cc3945266fda60ce15e64
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-03-14 17:36:53 -0700

    adding new colors

--- projects/colors/src/views/Home.vue
+++ projects/colors/src/views/Home.vue
@@ -11,6 +11,7 @@
             Color(:value=['#452393', '#FC3768', '#1FBCD4'])
             Color(:value=['#DD6463', '#027DB9', '#FFFAC5'])
             Color(:value=['#0D5B58', '#D46A1F', '#F4BB12', '#511422'])
+            Color(:value=['#50ae96', '#3c78d8'])
             #Type(value="Hello world", font="Roboto")
             #Type(value="Nice to meet you", font="Open Sans")
             #Type(value="hi hi hi hi hi Nice to meet you", font="Open Sans")
