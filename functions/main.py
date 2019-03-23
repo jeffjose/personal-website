a6be7626112b7c15f3aff809a6071f303ad7bf38
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-03-22 17:10:41 -0700

    making python cloud functions + firestore work

--- functions/main.py
+++ functions/main.py
@@ -2,11 +2,14 @@ import requests
 from google.cloud import firestore
 
 db = firestore.Client()
-users_ref = db.collection(u'colors')
+colors_ref = db.collection(u'colors')
+
+colors = colors_ref.get()
 
 def endpoint(request):
     return "Hello World!"
 
-def colors(request):
-    return str([x for x in users_ref.get()])
+def getcolors(request):
+
+    return ", ".join(["{} => {}".format(x.id, x.to_dict()) for x in colors])
 
