815f52d1f9a8a4ca7480c561ef9351024abdeb36
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-04-01 16:02:02 -0700

    toolbar update

--- projects/blog/src/components/Toolbar.vue
+++ projects/blog/src/components/Toolbar.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   div.toolbar
-    p Jeffrey
+    div.logo
+    p Jeffrey Jose
 </template>
 
 <script>
@@ -10,11 +11,23 @@ export default {
 </script>
 
 <style scoped lang="sass">
+
+@import url('https://fonts.googleapis.com/css?family=Roboto')
+
 .toolbar
   height: 3rem
   box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.07)
 
+  display: grid
+  grid-template-columns: 1fr [content-start] minmax(auto, 15rem) [word-start] 43rem [word-end] minmax(auto, 15rem) [content-end] 1fr
+
+
   p
+    grid-column: word
+
     padding: 0
     margin: 0
+    align-self: center
+    font-size: 1.34rem
+
 </style>
