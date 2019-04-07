86d90e8fff0f19a0b4af5b4c55d1216d221df33e
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-04-06 18:29:00 -0700

    blog home is responsive now

--- projects/blog/src/components/PostItemSmall.vue
+++ projects/blog/src/components/PostItemSmall.vue
@@ -119,8 +119,7 @@ $green: #34a853
 
    &::v-deep .wrapper
      display: grid
-     //grid-template-columns: 5fr [content-start] 2fr [word-start] 720px [word-end] 2fr [content-end] 5fr
-     grid-template-columns: 5fr [content-start] 2fr [word-start] 350px 20px 350px [word-end] 2fr [content-end] 5fr
+     grid-template-columns: 5fr [content-start] 2fr [word-start] 720px [word-end] 2fr [content-end] 5fr
      grid-template-rows: [top-start] auto [top-end middle-start] auto [middle-end bottom-start] auto [bottom-end]
 
      color: $text-color
@@ -128,7 +127,7 @@ $green: #34a853
 
 
      @media (max-width: 800px)
-       grid-template-columns: 20px [content-start] 2fr [word-start] auto [word-end] 2fr [content-end] 20px
+       grid-template-columns: 20px [content-start] 2fr [word-start] minmax(auto, 720px) [word-end] 2fr [content-end] 20px
 
      ::selection
        background: lighten($accent-color, 40%)
