a9da17d60b00646aa95c9182bb4bb82facdd981a
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-04-07 15:01:42 -0700

    Setting up homepage to have blog pt2

---
+++ projects/blog/src/views/Home.vue
@@ -0,0 +1,90 @@
+<template lang="pug">
+  div
+    background
+    div.wrapper
+      img.logo(src="/homepage/android-chrome-192x192.png")
+      div.header
+        div.title
+          span.name Jeffrey Jose
+          span.desc Product Manager at Google
+</template>
+
+<script>
+import Background from "@/components/Background.vue";
+
+export default {
+  name: "home",
+  created() {
+    document.title = document.location.hostname;
+  },
+  components: {
+    Background
+  }
+};
+</script>
+
+<style scoped lang="sass">
+
+$text-color: rgba(0, 0, 0, .84)
+$blockquote-color: lighten($text-color, 10%)
+$gray-color: lighten($blockquote-color, 35%)
+
+.wrapper
+  display: grid
+  grid-template-columns: 1fr [logo-start] 2fr [logo-end main-start] 9fr [main-end] 1fr
+  grid-template-rows: 2fr [logo-start main-start] 1fr [logo-end main-end] 2fr
+  height: 100vh
+  pointer-events: none
+  user-select: none
+  align-items: center
+
+  @media (max-width: 500px)
+    grid-template-columns: 1fr [logo-start main-start] 9fr [logo-end main-end] 1fr
+    grid-template-rows: 20px [logo-start] 1fr [logo-end main-start] 1fr [main-end] 6fr
+
+    .logo
+      justify-self: center
+
+    .header
+      justify-self: center
+      padding: 0
+      margin: 0
+
+      .title
+        justify-items: center
+        align-items: center
+
+.header
+  grid-column: main
+  grid-row: main
+  padding-left: 1rem
+  margin-top: 5px
+  display: grid
+  align-items: center
+
+.title
+  font-family: 'Lato', sans-serif
+  font-weight: 400
+  display: grid
+  grid-template-columns: 1fr
+  grid-template-rows: 1fr 1fr
+  align-items: center
+  position: relative
+
+  & .name
+    font-size: 1.4rem
+    color: $text-color
+    letter-spacing: 1px
+    text-transform: uppercase
+
+  & .desc
+    font-size: 1rem
+    color: $gray-color
+
+.logo
+  height: 4rem
+  width: 4rem
+  grid-row: logo
+  grid-column: logo
+  justify-self: end
+</style>
