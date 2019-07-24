5843e69aa201851647c1369a56df543a7c26631e
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-07-24 15:30:56 -0700

    adding timeago

--- server/app.py
+++ server/app.py
@@ -1,13 +1,19 @@
 import os
+import datetime, timeago
 
 from flask import Flask
+
+start = datetime.datetime.now()
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    target = os.environ.get('TARGET', 'World')
-    return 'Hello {}!\n'.format(target)
+    now = datetime.datetime.now()
+
+    ago = timeago.format(start, now)
+
+    return 'Started {}\n. Hello World!\n'.format(ago)
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True,host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
