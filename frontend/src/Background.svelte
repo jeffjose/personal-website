dc0ddacc54aafaf320623fc6a87617541c12afe6
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-12-01 18:11:32 -0800

    grow on click

--- frontend/src/Background.svelte
+++ frontend/src/Background.svelte
@@ -1,7 +1,9 @@
 <script>
-import {writable} from "svelte/store"
+import { writable } from "svelte/store"
+import { spring } from "svelte/motion"
 
 let coords = writable({x: 10, y:10})
+let size = spring(0)
 </script>
 
 
@@ -16,8 +18,11 @@ circle
 
 </style>
 
-<svg on:mousemove="{e => coords.set({x:e.clientX, y:e.clientY})}">
+<svg
+  on:mousemove="{e => coords.set({x:e.clientX, y:e.clientY})}"
+  on:mousedown="{() => size.set(60)}"
+  >
   <g id="wrapper">
-    <circle r="10" cx="{$coords.x}"  cy="{$coords.y}"  fill="red">
+    <circle cx="{$coords.x}" cy="{$coords.y}" r="{$size}" fill="red">
   </g>
 </svg>
