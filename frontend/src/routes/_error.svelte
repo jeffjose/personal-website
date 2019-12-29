bb8cfa0c5f23b1f4aeca658e1c819675371915a3
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-12-28 16:35:18 -0800

    slightly better 404

--- frontend/src/routes/_error.svelte
+++ frontend/src/routes/_error.svelte
@@ -1,4 +1,7 @@
 <script>
+  import Header from '../components/Header.svelte';
+  import Footer from '../components/Footer.svelte';
+
 	export let status;
 	export let error;
 
@@ -6,35 +9,27 @@
 </script>
 
 <style>
-	h1, p {
-		margin: 0 auto;
-	}
 
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  .contents {
+    flex: 1;
+  }
 </style>
 
 <svelte:head>
 	<title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
+<template lang="pug">
+Header
 
-<p>{error.message}</p>
+.contents
+  center
+    h1 {status}
+    | {error.message}
 
-{#if dev && error.stack}
-	<pre>{error.stack}</pre>
-{/if}
+    +if('dev && error.stack')
+      pre {error.stack}
+
+Footer
+</template>
+
