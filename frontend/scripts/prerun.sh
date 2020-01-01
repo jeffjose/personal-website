090e2063e85fa45724bb9bf599adccf598994735
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-12-31 22:18:42 -0800

    adding a comment

--- frontend/scripts/prerun.sh
+++ frontend/scripts/prerun.sh
@@ -8,6 +8,7 @@ pip install -r ./scripts/requirements
 # Build index for blogs
 ./scripts/build_blog_index.py
 
+# TODO: Is this really needed when run in cloud-build?
 # Create adocs and build book index
 ./scripts/adoc_from_bookcovers.py
 
