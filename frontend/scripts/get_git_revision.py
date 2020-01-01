4021fa530eab62d3ce5904d559048f50b2421a4c
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-12-31 16:05:47 -0800

    use prod as tag on cloud-build

--- frontend/scripts/get_git_revision.py
+++ frontend/scripts/get_git_revision.py
@@ -20,7 +20,8 @@ def run(cmd):
     # In cloud-build, we have the same directory structure but no .git.
     # That info is passed in via ENV variables
     if BUILD_CTX == 'cloud-build':
-        TAG_NAME = os.environ.get('TAG_NAME', 'defaulttag')
+        TAG_NAME = os.environ.get('TAG_NAME', 'defaulttag') if os.environ.get(
+            'TAG_NAME', 'defaulttag') else "prod"
         SHORT_SHA = os.environ.get('SHORT_SHA', 'defaultsha')
 
         return f'{TAG_NAME}-0-g{SHORT_SHA}'
