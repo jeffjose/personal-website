86cbaa22679217859a8f48aeab797245fa946b10
Jeffrey Jose <jeffjosejeff@gmail.com>
2021-06-14 22:11:25 -0700

    Nav --> Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,6 @@
+
+<header>
+  <div class="flex max-w-screen-lg px-2 pt-10 pb-4 mx-auto border-b-2 border-gray-700 cursor-default xl:max-w-screen">
+    <p class="text-2xl tracking-wider font-lato hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-blue-500 hover:via-pink-500 hover:to-rose-500 ">JEFFREY JOSE</p>
+  </div>
+</header>
--- src/components/Nav.svelte
+++
@@ -1,8 +0,0 @@
-
-<header>
-  <div class="max-w-screen-lg xl:max-w-screen mx-auto px-2 pt-10 flex cursor-default pb-4 border-b-2 border-rose-400">
-    <p class="tracking-wider font-lato text-2xl
-    hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-blue-500 hover:via-pink-500 hover:to-rose-500
-    ">JEFFREY JOSE</p>
-  </div>
-</header>
