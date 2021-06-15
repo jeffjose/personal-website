94370b63d2a2e570257aa6c01b805ed5b3706c2e
Jeffrey Jose <jeffjosejeff@gmail.com>
2021-06-14 22:38:26 -0700

    header and footer extends full width

---
+++ src/components/Footer.svelte
@@ -0,0 +1,8 @@
+
+<footer>
+  <div class="flex px-2 pt-4 pb-10 border-t-2 cursor-default max-w-screen h-80 bg-rose-900">
+  </div>
+  <div class="grid py-3 mx-auto bg-gray-900 max-w-screen justify-items-end">
+    <p class="mr-4 tracking-wider text-white font-lato">JEFFREY JOSE</p>
+  </div>
+</footer>
