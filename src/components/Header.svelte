0b5d5358f2003ad5c7b843b8fea842e60d79f9b5
Jeffrey Jose <jeffjosejeff@gmail.com>
2021-06-17 23:23:10 -0700

    WIP: dummy elements

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,6 @@
 
 <header>
-  <div class="flex px-2 pt-10 pb-4 mx-auto border-b-2 border-gray-700 cursor-default md:pl-60 max-w-screen">
+  <div class="flex px-2 pt-10 pb-4 mx-auto border-b-0 border-gray-700 cursor-default md:pl-60 max-w-screen">
     <p class="text-2xl tracking-wider font-lato hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-blue-500 hover:via-pink-500 hover:to-rose-500 ">JEFFREY JOSE</p>
   </div>
 </header>
